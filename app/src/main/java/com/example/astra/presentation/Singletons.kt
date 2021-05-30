7d46d2172b30065f023dc76aeda40b1361572fe7
NicoLQ <2000nicolas@gmail.com>
2021-05-30 03:05:09 +0200

    le cache hahahaa

--- app/src/main/java/com/example/astra/presentation/Singletons.kt
+++ app/src/main/java/com/example/astra/presentation/Singletons.kt
@@ -1,16 +1,28 @@
 package com.example.astra.presentation
 
+import com.example.astra.presentation.PlanetApplication.Companion.context
 import com.example.astra.presentation.api.PlanetApi
+import okhttp3.Cache
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
 
 class Singletons{
     companion object{
+        var cache = Cache(File(context?.cacheDir, "reponses"), 10*1024*1024)
+
+        val okhttpClient: OkHttpClient = OkHttpClient().newBuilder()
+                .cache(cache)
+                .build()
 
         val planetApi: PlanetApi = Retrofit.Builder()
                 .baseUrl("https://api.le-systeme-solaire.net/rest/")
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(okhttpClient)
                 .build()
                 .create(PlanetApi::class.java)
+
+
     }
 }
